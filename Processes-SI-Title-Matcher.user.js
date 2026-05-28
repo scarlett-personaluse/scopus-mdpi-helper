e6535b4df205aab951f181cabeba4138d4e5b486
scarlett-personaluse <1350319238@qq.com>
2026-05-28 17:38:09 +0800

    Update Processes-SI-Title-Matcher.user.js

--- Processes-SI-Title-Matcher.user.js
+++ Processes-SI-Title-Matcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Processes SI Title Matcher
 // @namespace    Processes-SI-Title-Matcher
-// @version      4.1
+// @version      4.2
 // @author       Jiali Tang
 // @icon         https://pub.mdpi-res.com/img/journals/processes-logo-sq.png?1e142e5ab0d148f8
 // @description  Match selected scholar information with existing Processes SI titles, generate SI titles, Scilit queries, keyword lists, and clean pasted text
